85a93a4175b63eac81858a2f7d4cef3f3341f768
sergiikonov <sergii2konovalov@gmail.com>
2025-10-31 15:56:13 +0200

    complete task

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final int THRESHOLD = 10;
+
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +15,22 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
+        long distance = finishPoint - startPoint;
+        long result = 0;
         // write your code here
-        return null;
+        if (distance <= THRESHOLD) {
+            for (long i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        } else {
+            int mid = startPoint + (finishPoint - startPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, mid);
+            MyTask rightTask = new MyTask(mid, finishPoint);
+            leftTask.fork();
+            long rightResult = rightTask.compute();
+            long leftResult = leftTask.join();
+            result = leftResult + rightResult;
+        }
+        return result;
     }
 }
